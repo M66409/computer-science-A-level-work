b79e4fc39621dbc6f966efd0d17851634eacff90
M66409 <144997570+M66409@users.noreply.github.com>
2024-04-29 11:11:03 +0100

    Update pointer_testing.cpp

--- In class coding work/Random work/pointer_testing.cpp
+++ In class coding work/Random work/pointer_testing.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <chrono>
 int func(int *x){
     int s=*x;
     std::cout << x <<"\n";
@@ -8,17 +8,67 @@ int func(int *x){
 }
 
 int otherfunc(int x){
+     std::cout << &x << "\n";
      x=x+1;
-     std::cout<< &x <<"\n";
      return x;
 }
+
+int* test( int* x){
+    std::cout << x <<"\n";
+    int* y = x+1;
+    int* z = x-1;
+    std::cout << y <<"\n";
+    std::cout << z <<"\n";
+    std::cout << *x << "\n";
+    std::cout << *y << "\n";
+    std::cout << *z << "\n";
+    return y;
+}
+
+void loop( int* x){
+    int i =100;
+    while (i>0){
+        i--;
+        std::cout << *x << "\n";
+        x--;
+    }
+    std::cout << "\n";
+    
+    i = 100;
+    while(i<200){
+        i++;
+        std::cout << *x << "\n";
+        x++;
+    }
+}
+
+
 int main() {
-    int y =5;
+    
+    int arr[10]={42,763,126,3,751,126,2,545,7654};
+    int* w = arr;
+    int i = 0;
+    while(i<9){
+        w++;
+        i++;
+        std::cout << (w-1) << "\n";
+        std::cout << *(w-1) << "\n"; 
+    }
+    
+    for(int i=0;i<9;i++){
+        std::cout << arr[i] << "\n";
+        std::cout << &arr[i] << "\n";
+    }
+    /*int y =5;
     int* w = &y;
-    std::cout << w <<"\n";
+    loop(w);
+    /*std::cout << w <<"\n";
+    
     int z = func(w);
-    std::cout << z << "\n";
+    //std::cout << z << "\n";
+    
     int v = otherfunc(y);
-    std::cout << v;
+    //std::cout << v;
+    */
     return 0;
 }
